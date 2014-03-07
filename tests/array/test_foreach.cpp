f47c051a47ab0af9244bbac7bdf3ee03af1cc0eb
Mark Wiebe <mwiebe@continuum.io>
2014-03-07 02:10:13 -0800

    Fix up nd::foreach so the prototype works, including a small test

--- tests/array/test_foreach.cpp
+++ tests/array/test_foreach.cpp
@@ -25,4 +25,19 @@ TEST(ArrayViews, IntFunc) {
 
     c = nd::foreach(a, b, intfunc);
     EXPECT_EQ(-20, c.as<int>());
+
+    int aval0[2][3] = {{0, 1, 2}, {5, 6, 7}};
+    int bval0[3] = {5, 2, 4};
+    a = aval0;
+    b = bval0;
+    c = nd::foreach(a, b, intfunc);
+    EXPECT_EQ(ndt::type("strided * strided * int32"), c.get_type());
+    ASSERT_EQ(2, c.get_shape()[0]);
+    ASSERT_EQ(3, c.get_shape()[1]);
+    EXPECT_EQ(-10, c(0,0).as<int>());
+    EXPECT_EQ(-2, c(0,1).as<int>());
+    EXPECT_EQ(-4, c(0,2).as<int>());
+    EXPECT_EQ(0, c(1,0).as<int>());
+    EXPECT_EQ(8, c(1,1).as<int>());
+    EXPECT_EQ(6, c(1,2).as<int>());
 }

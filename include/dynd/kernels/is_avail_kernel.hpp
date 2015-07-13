e41420c84d268eec677edfee68c17dacc2e08430
izaid <hi@irwinzaid.com>
2015-07-13 19:49:02 +0100

    Trying another fix for MSVC #2

--- include/dynd/kernels/is_avail_kernel.hpp
+++ include/dynd/kernels/is_avail_kernel.hpp
@@ -365,7 +365,8 @@ namespace nd {
 namespace ndt {
 
   template <type_id_t Src0ValueTypeID>
-  struct type::equivalent<nd::is_avail_kernel<Src0ValueTypeID>> {
+  struct type::equivalent<nd::detail::is_avail_kernel<
+      Src0ValueTypeID, type_kind_of<Src0ValueTypeID>::value>> {
     static type make()
     {
       return arrfunc_type::make(

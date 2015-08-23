140f3b0c047b5a62d7bc9d463c35fcf37974ac03
izaid <hi@irwinzaid.com>
2015-08-23 22:45:46 +0100

    Removed destruct_children entirely

--- include/dynd/kernels/base_kernel.hpp
+++ include/dynd/kernels/base_kernel.hpp
@@ -143,15 +143,7 @@ namespace nd {
     static void destruct(ckernel_prefix *rawself)
     {
       SelfType *self = get_self(rawself);
-      /* If there are any child kernels, a child class must implement */
-      /* this to destroy them. */
-      self->destruct_children();
       self->~SelfType();
-    }
-
-    /**  Default implementation of destruct_children does nothing.  */
-    void destruct_children()
-    {
     }
 
     static intptr_t instantiate(
